f3a8a9af86c8c31e05ed0143659be53b875c8b16
ChrisNakayama <88105917+ChrisNakayama@users.noreply.github.com>
2022-01-15 11:22:42 -0800

    Edit HTML and checked webpack.

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: 'Doom Watch',
+      title: 'FridayProjectSix',
       template: './src/index.html',
       inject: 'body'
     }),
